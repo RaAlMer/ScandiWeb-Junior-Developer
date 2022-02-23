925e68f793a00a4be73cd75a3d3197250488e981
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2022-02-23 18:30:36 +0100

    done

--- database/products.sql
+++ database/products.sql
@@ -1,3 +1,14 @@
+--
+-- Credentials
+--
+
+--
+-- 'server' => 'localhost'
+-- 'user' => 'root'
+-- 'password' => ''
+-- 'database' => 'products'
+--
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
