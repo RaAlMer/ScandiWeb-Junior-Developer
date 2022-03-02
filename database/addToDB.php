080d4b10c39a2f714d111dc1d1d6fe18c286875c
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2022-03-02 15:26:19 +0100

    Removed conditionals

--- database/addToDB.php
+++ database/addToDB.php
@@ -1,38 +1,35 @@
 <?php
-    require("../models/classProduct.php");
+    require('../models/classProduct.php');
     require("../models/classBook.php");
     require("../models/classDvd.php");
     require("../models/classFurniture.php");
     require_once('../database/connection.php');
 
     try {
-      //Variables with POST[input]
+      //assign input value's to variables
       $sku = $_POST['sku'];
       $name = $_POST['name'];
       $price = $_POST['price'];
       $type = $_POST['productType'];
+      
+      $weight = $_POST['weight'];
+      
+      $size = $_POST['size'];
+      
+      $height = $_POST['height'];
+      $width = $_POST['width'];
+      $length = $_POST['length'];
+      
+      $validators = [
+        'dvd' => 'DVD',
+        'book' => 'Book',
+        'furniture' => 'Furniture',
+      ];
 
-      switch ($type) {
-        case 'furniture':
-        $height = $_POST['height'];
-        $width = $_POST['width'];
-        $length = $_POST['length'];
-        $furni = new Furniture($sku,$name,$price,$height,$width,$length,$type);
-        $furni->setFurniture($connection,$sku,$name,$price,$height,$width,$length,$type);
-        break;
+      $validatorClass = $validators[$type];
+      $validator = new $validatorClass($sku, $name, $price, $size, $weight, $height, $width, $length);
+      $validator->validate($connection, $sku, $name, $price, $size, $weight, $height, $width, $length, $type);
 
-        case 'book':
-        $weight = $_POST['weight'];
-        $book = new Book($sku,$name,$price,$weight,$type);
-        $book->setBook($connection,$sku,$name,$price,$weight,$type);
-        break;
-
-        case 'dvd':
-        $size = $_POST['size'];
-        $dvd = new DVD($sku,$name,$price,$size,$type);
-        $dvd->setDVD($connection,$sku,$name,$price,$size,$type);
-        break;
-      }
 
       header("refresh:0;url=/");
 
