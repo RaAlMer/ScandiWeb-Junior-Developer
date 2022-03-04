83d6861be11ec6f88c2ed06581bff5617782fc36
Raúl Alonso <67586098+RaAlMer@users.noreply.github.com>
2022-03-04 20:16:30 +0100

    Update classFurniture.php

--- models/classFurniture.php
+++ models/classFurniture.php
@@ -18,18 +18,6 @@
       $this->width = $width;
       $this->length = $length;
     }
-
-    //Creates a furniture record in the database
-    function validate($connection, $sku, $name, $price, $size, $weight, $height, $width, $length, $type) {
-      try {
-        $sql = $connection->query("INSERT INTO `products` (`sku`, `name`, `price`, `height`, `width`, `length`,`type`)  VALUES ('$sku', '$name', $price, $height, $width, $length, '$type');");
-
-        return $sql;
-
-      } catch (PDOException $error) {
-        echo $error->getMessage();
-      }
-    }
     
     //Gets all furniture records in the database
     protected function getFurniture($connection){
